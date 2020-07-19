f6bfe304b7e5633ccf5efb5e4d9bd10efc3bff1c
Furkan Güneştaş <37254602+fgunestas@users.noreply.github.com>
2020-07-19 19:59:21 +0300

    Add files via upload

--- sample_uav.py
+++ sample_uav.py
@@ -72,7 +72,7 @@ class SampleUAV(BaseUAV):
         if self.fallback==False:
             knot=20/3
             dist= util.dist(self.start_loc,[self.uav_msg['active_uav']['location'][0],self.uav_msg['active_uav']['location'][1]])
-            dist=dist/1852 #knot to kmh
+            dist=dist*1.852 #knot to kmh
             fuel_=dist*knot #aradaki knot mesafe * knot basina harcanan yakit.
 
             if fuel>fuel_:
